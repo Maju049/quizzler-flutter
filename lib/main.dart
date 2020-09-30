1d18405ed555453124d613b7076e3b16cf80212f
Maju049 <64065807+Maju049@users.noreply.github.com>
2020-09-30 20:23:27 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,7 +26,7 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<String> qsutvView = [
+  /*List<String> qsutvView = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.',
@@ -47,7 +48,17 @@ class _QuizPageState extends State<QuizPage> {
     false,
     true,
     true,
+  ];*/
+
+  List<Question> qustans = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.',a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.',a: true),
+    Question(q: 'A slug\'s blood is green.',a: true),
+    Question(q: '1 + 1 = 2',a: true),
+    Question(q: '2 + 2 = 5',a: false),
+    Question(q: '2 + 2 = 4',a: true),
   ];
+
   int trackClick = 0;
 
   List<Widget> answertrack = [];
@@ -65,7 +76,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                qsutvView[trackClick],
+                qustans[trackClick].qstText,//qsutvView[trackClick],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -89,7 +100,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool truech = answer[trackClick];
+                bool truech = qustans[trackClick].answ; //answer[trackClick];
                 if(truech == true){
                   setState(() {
                     answertrack.add(Icon(Icons.check,color: Colors.greenAccent,),);
@@ -118,7 +129,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool truech = answer[trackClick];
+                bool truech = qustans[trackClick].answ;//answer[trackClick];
                 if(truech == false){
                   setState(() {
                     answertrack.add(Icon(Icons.check,color: Colors.greenAccent,),);
